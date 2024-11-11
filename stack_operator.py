8be778cdf31bf5ceef3f7579df3d9d5a5dbb613b
seonghwan <wkrdmsxowkd@naver.com>
2024-11-11 10:10:53 +0900

    Modify script verification method

--- stack_operator.py
+++ stack_operator.py
@@ -74,9 +74,8 @@ class Stack:
             return None
         pubKey = self.POP() # Public Key
         sig = self.POP() # Signature
-        tx_hash = find_tx_hash(txid) # tx값에 hash함수 적용해서 return 하는 find_tx_hash 함수 적용
         try:
-            pubKey.verify(sig, tx_hash) # 검증이 완료됐다면 stack에 True를 PUSH
+            pubKey.verify(sig, txid) # 검증이 완료됐다면 stack에 True를 PUSH
             self.PUSH(True)
         except Exception as e:
             self.PUSH(False) # 검증이 실패했다면 stack에 False를 PUSH
@@ -87,9 +86,8 @@ class Stack:
             return None
         pubKey = self.POP() # Public Key
         sig = self.POP() # Signature
-        tx_hash = find_tx_hash(txid) # tx값에 hash함수 적용해서 return 하는 find_tx_hash 함수 적용
         try:
-            pubKey.verify(sig, tx_hash) # 검증이 완료됐다면 오류없이 코드 종료
+            pubKey.verify(sig, txid) # 검증이 완료됐다면 오류없이 코드 종료
         except Exception as e:
             print("CHECKSIGVERIFY failed", str(e)) # 검증이 실패했다면 오류 발생
         
@@ -100,15 +98,14 @@ class Stack:
         sig_list = [self.POP() for _ in range(M)]  # M개의 Signature를 stack에서 꺼내서 저장
         
         combinations_list = list(combinations(pubKey_list, M))
-        tx_hash = find_tx_hash(txid)
-
+        
         for comb in combinations_list:
             count = 0 # M개의 sig-pubKey 쌍이 일치하는지 count
             match = False
             for sig in sig_list:
                 for pubKey in comb:
                     try:
-                        pubKey.verify(sig, tx_hash) 
+                        pubKey.verify(sig, txid) 
                         count += 1 # 검증이 성공했다면 count += 1
                         break  # 검증이 성공하면 해당 Public Key 조합 내 다른 Public Key는 건너뜀
                     except Exception as e:
@@ -128,7 +125,6 @@ class Stack:
         sig_list = [self.POP() for _ in range(M)]  # M개의 Signature를 stack에서 꺼내서 저장
         
         combinations_list = list(combinations(pubKey_list, M))
-        tx_hash = find_tx_hash(txid)
 
         for comb in combinations_list:
             count = 0 # M개의 sig-pubKey 쌍이 일치하는지 count
@@ -136,7 +132,7 @@ class Stack:
             for sig in sig_list:
                 for pubKey in comb:
                     try:
-                        pubKey.verify(sig, tx_hash) 
+                        pubKey.verify(sig, txid) 
                         count += 1 # 검증이 성공했다면 count += 1
                         break  # 검증이 성공하면 해당 Public Key 조합 내 다른 Public Key는 건너뜀
                     except Exception as e:
@@ -210,11 +206,3 @@ class Stack:
                 i += 1
                 
         return self
-
-
-def find_tx_hash(txid):
-    with open('/data/mempool.txt', 'r') as mempool: # mempool에서 utxo를 포함하는 tx의 내용 찾기
-        tx_data = mempool.read().split('\n\n') # Blank line으로 구분해서 저장한 후
-        tx = [tx for tx in tx_data if tx_data.split('\n')[0] == txid] # txid가 일치하는 tx 내용을 저장 
-        tx_hash = hashlib.sha256(tx[0].encode('utf-8')).hexdigest() # hash값으로 변환
-        return tx_hash
